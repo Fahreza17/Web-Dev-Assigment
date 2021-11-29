5d61af5b24ad894f10a41b4e7a71794d6996d438
unknown <indminecraft52@gmail.com>
2021-11-29 09:52:06 +0800

    CRUD menggunakn laravel

---
+++ laravel-crud/database/migrations/2021_11_28_093140_create_mahasiswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Testing\Constraints\SoftDeletedInDatabase;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('nim');
+            $table->string('nama');
+            $table->longText('alamat');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
